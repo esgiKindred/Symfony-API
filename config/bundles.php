a940e61c456e5486edcce7c85b1c474c1a28c66a
Pierre Landry <45065999+Linkale@users.noreply.github.com>
2022-04-08 12:40:33 +0200

    bundles

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     SymfonyBundles\JsonRequestBundle\JsonRequestBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
