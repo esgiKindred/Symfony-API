13e06ae073c802022b136b55d503df66b1bd593a
Carlitos73490 <71330622+Carlitos73490@users.noreply.github.com>
2022-06-20 18:56:46 +0200

    [Add] Id in JWT Token

---
+++ src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class JWTCreatedListener
+{
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        $user = $event->getUser();
+
+        $payload = $event->getData();
+        $payload['id'] = $user->getId();
+
+        $event->setData($payload);
+    }
+}
